416934918c22a37cb8bee96a96636f1a3093db67
jGeli <jaybee.aicpa@gmail.com>
2023-04-11 12:43:25 +0800

    topology

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 //Initialize MongoDB
 mongoose.connect('mongodb://localhost:27017/phlebotomy-api', {
   useNewUrlParser: true,
-//  useUnifiedTopology: true,
+ useUnifiedTopology: true,
 })
 .then(() => {
   console.log('Connected to MongoDB');
