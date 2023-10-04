5ecbf31b9a7250e060303799a55097be0be28a46
jGeli <jaybee.aicpa@gmail.com>
2023-10-04 20:03:16 +0800

    change port to 5005

--- triggers.js
+++ triggers.js
@@ -4,8 +4,8 @@ const axios = require('axios')
 
 cron.schedule('* * * * *', async () => {
 	try {
-		await axios.get(`http://localhost:${process.env.PORT}/api/v1/samsara/driver/vehicle/assignments?filterBy=drivers`)
-		await axios.get(`http://localhost:${process.env.PORT}/api/v1/samsara/vehicle/stats`)
+		await axios.get(`http://localhost:5005/api/v1/samsara/driver/vehicle/assignments?filterBy=drivers`)
+		await axios.get(`http://localhost:5005/api/v1/samsara/vehicle/stats`)
 	} catch (error) {
 		console.log(error)
 	}
