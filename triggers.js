126ef47ce61cb7efddd6d76f74ffd81d187290d1
jGeli <jaybee.aicpa@gmail.com>
2023-10-04 19:53:24 +0800

    replace axios url at cron

--- triggers.js
+++ triggers.js
@@ -4,8 +4,8 @@ const axios = require('axios')
 
 cron.schedule('* * * * *', async () => {
 	try {
-		await axios.get('http://localhost:5001/api/v1/samsara/driver/vehicle/assignments?filterBy=drivers')
-		await axios.get('http://localhost:5001/api/v1/samsara/vehicle/stats')
+		await axios.get(`${process.env.PORT}/api/v1/samsara/driver/vehicle/assignments?filterBy=drivers`)
+		await axios.get(`${process.env.PORT}/api/v1/samsara/vehicle/stats`)
 	} catch (error) {
 		console.log(error)
 	}
